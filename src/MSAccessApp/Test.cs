065ed440b5fc3bdaf60c1c533264b1873319e921
t2wain <11613168+t2wain@users.noreply.github.com>
2024-01-01 08:14:39 -0600

    Initial commit

---
+++ src/MSAccessApp/Test.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Office.Interop.Access.Dao;
+using MSAccessLib;
+
+namespace MSAccessApp
+{
+    public class Test
+    {
+        public void Run()
+        {
+            string f1 = @"c:\dev\Routing.accdb";
+            string f2 = @"c:\dev\RoutingLink.accdb";
+            string f3 = @"c:\dev\TestNewDB.accdb";
+            //OpenMSAccess(f2);
+            //OpenMSAccess(f1);
+            //OpenExcel();
+            //OpenODBC();
+            //PrintAccessTables(f2);
+            //CreateMSAccess(f3);
+            //LinkTables(f2);
+            ImportTables(f1);
+        }
+
+        protected void OpenMSAccess(string fileName)
+        {
+            using var t = new DB();
+            Database? db = null;
+            try
+            {
+                db = t.OpenAccessDB(fileName);
+                t.PrintDatabase(db, false);
+            }
+            finally { db?.Close(); }
+        }
+
+        protected void OpenExcel()
+        {
+            using var t = new DB();
+            Database? db = null;
+            try
+            {
+                db = t.OpenExcel(@"c:\dev\SampleTable.xlsx");
+                t.PrintDatabase(db, true);
+            }
+            finally { db?.Close(); }
+        }
+
+        protected void OpenODBC()
+        {
+            using var t = new DB();
+            Database? db = null;
+            try
+            {
+                db = t.OpenODBC(@"RoutingDB");
+                //t.PrintDatabase(db, true);
+            }
+            finally { db?.Close(); }
+        }
+
+        protected void CreateMSAccess(string fileName)
+        {
+            using var t = new DB();
+            Database? db = null;
+            try
+            {
+                db = t.CreateMSAccess(fileName);
+            }
+            finally { db?.Close(); }
+        }
+
+        protected void PrintAccessTables(string fileName)
+        {
+            using var t = new DB();
+            Database? db = null;
+            try
+            {
+                db = t.OpenAccessDB(fileName);
+                t.PrintDatabaseTables(db, true);
+            }
+            finally { db?.Close(); }
+        }
+
+
+        protected void LinkTables(string fileName)
+        {
+            using var t = new DB();
+            string testDb = @"c:\dev\TestLinkDB.accdb";
+            if (File.Exists(testDb))
+                File.Delete(testDb);
+            Database? destDB = null;
+            Database? srcDB = null;
+            try
+            {
+                destDB = t.CreateMSAccess(testDb);
+                srcDB = t.OpenAccessDB(fileName);
+                t.LinkTables(destDB!, srcDB!, t => t.Attributes == 0 || t.Attributes == 1073741824);
+            }
+            finally
+            {
+                destDB?.Close();
+                srcDB?.Close();
+            }
+        }
+
+        protected void ImportTables(string fileName)
+        {
+            using var t = new DB();
+            string testDb = @"c:\dev\TestLinkDB.accdb";
+            if (File.Exists(testDb))
+                File.Delete(testDb);
+            Database? destDB = null;
+            Database? srcDB = null;
+            try
+            {
+                destDB = t.CreateMSAccess(testDb);
+                srcDB = t.OpenAccessDB(fileName);
+                t.ImportTables(destDB!, srcDB!, t => t.Attributes == 0 || t.Attributes == 1073741824);
+            }
+            finally
+            {
+                destDB?.Close();
+                srcDB?.Close();
+            }
+        }
+    }
+}
---
+++ src/MSAccessLib/Connect.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Office.Interop.Access.Dao;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MSAccessLib
+{
+    public static class Connect
+    {
+        public static string DSNFile(string dsnFileName)
+        {
+            return string.Format("ODBC;FILEDSN={0}", dsnFileName);
+        }
+
+        public static string DSN(string dsnName)
+        {
+            return string.Format("ODBC;DSN={0}", dsnName);
+        }
+
+        public static string Oracle(string server, string uid, string pwd)
+        {
+            // Driver={Microsoft ODBC for Oracle};Server=myServerAddress;Uid=myUsername;Pwd=myPassword;
+
+            // Driver={Microsoft ODBC for Oracle};
+            // Server=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=199.199.199.199)(PORT=1523))(CONNECT_DATA=(SID=dbName)));
+            // Uid=myUsername;Pwd=myPassword;
+
+            // Driver={Microsoft ODBC for Oracle};
+            // CONNECTSTRING=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=server)(PORT=7001))(CONNECT_DATA=(SERVICE_NAME=myDb)));
+            // Uid=myUsername;Pwd=myPassword;
+
+            return "ODBC;Driver={Microsoft ODBC for Oracle};" + string.Format("Server={0};Uid={1};Pwd={2};",
+                server, uid, pwd);
+        }
+
+        public static string SqlServer(string server, string dbName)
+        {
+            // Driver={SQL Server};Server=myServerAddress;Database=myDataBase;Trusted_Connection=Yes;
+
+            return "ODBC;Driver={SQL Server};" + string.Format("Server={0};Database={1};Trusted_Connection=Yes;",
+                server, dbName);
+        }
+
+        public static string Sqlite(string dbFileName)
+        {
+            // DRIVER=SQLite3 ODBC Driver;Database=c:\mydb.db;LongNames=0;Timeout=1000;NoTXN=0;SyncPragma=NORMAL;StepAPI=0;
+
+            return "ODBC;DRIVER=SQLite3 ODBC Driver;" + 
+                string.Format("Database={0};LongNames=0;Timeout=1000;NoTXN=0;SyncPragma=NORMAL;StepAPI=0;",
+                    dbFileName); 
+        }
+
+        public static string MSAccess(string filename, string pwd)
+        {
+            var c = new StringBuilder();
+            c.Append(string.Format("MS Access;DATABASE={0}", filename));
+            if (!string.IsNullOrWhiteSpace(pwd))
+                c.Append(string.Format(";PWD={0}", pwd));
+            return c.ToString();
+        }
+
+        public static string Excel(string filename)
+        {
+            var c = new StringBuilder();
+            c.Append(filename.EndsWith(".xlsx") ? "Excel 12.0;HDR=Yes;" : "Excel 8.0;HDR=Yes;");
+            if (!string.IsNullOrWhiteSpace(filename))
+                c.Append(string.Format("DATABASE={0};", filename));
+            return c.ToString();
+        }
+    }
+}
